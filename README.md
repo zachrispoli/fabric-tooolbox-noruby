dc5cc3bd26d565878706cff0b3a29e75acbe0ed0
Zach Rispoli <zach.rispoli@gmail.com>
2018-04-12 15:23:30 -0400

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# fabricjs-toolbox
+
+A library for creating toolbox GUIs for fabric.js.
+
+(Work in progress)
---
+++ tools/Zoom.js
@@ -0,0 +1,31 @@
+(function () {
+
+    window.ZoomTool = new PaperToolbox.Tool({
+        global: true,
+        onMouseDown : function (e) {
+            
+        },
+        onDoubleClick : function (e) {
+
+        },
+        onMouseMove : function (e) {
+            
+        },
+        onMouseDrag : function (e) {
+            
+        },
+        onMouseUp : function (e) {
+            
+        },
+        onSelected : function (e) {
+            
+        },
+        onDeselected : function (e) {
+
+        },
+        onScroll : function (e) {
+            console.log('onScroll')
+        },
+    });
+
+})();
